cffe4d28afb97134310cc67a093cd310e00cc84c
Gervan <gervan@users.noreply.github.com>
2020-06-19 16:25:22 -0400

    Updates

--- home.php
+++ home.php
@@ -7,3 +7,10 @@
     Password: <input type="text" name="password"><br /><br>
     <input type="submit" value="submit data" name="submit" />
 </form>
+<h1>OR</h1>
+<form action="connect.php" method="POST">
+    User Name: <input type="text" name="user"><br />
+    Password <input type="text" name="pass"><br />
+    <br>
+    <input type="submit" value="submit data" name="login" />
+</form>
