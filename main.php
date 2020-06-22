5c993baede3ac324d0e56eeea746228a833f80ba
lalaantika <sharmal0707@gmail.com>
2020-06-22 14:46:00 -0400

    sssion with query

--- main.php
+++ main.php
@@ -1,6 +1,18 @@
 <?php
+require_once("connect.php");
     session_start();
-    echo $_SESSION['user_id'];
+    $conn = new mysqli('localhost',$user, $pass, $db );
+    $query = "SELECT h.horse_name,h.horse_breed, u.username from user_horse uh 
+    JOIN horse h on h.horse_id =uh.horse_id join user u on uh.user_id = u.user_id 
+    where u.user_id ='".$_SESSION ['user_id']."'";
+    $result = $conn->query($query);
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+          echo  $dbusername=$row['username'];  
+          echo  $dbhorse_name=$row['horse_name']; 
+          echo  $dbhorse_breed=$row['horse_breed'];  
+        }
+        
     }
 ?>
 
