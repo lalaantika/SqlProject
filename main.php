96daaa1c777546a093ea1c30da55382ec6b4584f
lalaantika <sharmal0707@gmail.com>
2020-06-23 13:03:53 -0400

    my table

--- main.php
+++ main.php
@@ -7,25 +7,31 @@ require_once("connect.php");
     JOIN horse h on h.horse_id =uh.horse_id join user u on uh.user_id = u.user_id 
     where u.user_id ='".$_SESSION ['user_id']."'";
     $result = $conn->query($query);
+    echo "Horse list";
+    echo "<table>
+        <tr>
+        <th>Username</th>
+        <th>Horsename</th>
+        <th>Horse Breed</th>
+        <th>Skill</th>
+        <th>Horse Age</th>
+        <th>Horse Color</th>
+        <th>Horse ID</th>
+        <th>Action</th>
+        </tr>";
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-          echo  $dbusername=$row['username'] ; 
-          echo "<br>"; 
-          echo  $dbhorse_name=$row['horse_name'];
-          echo "<br>";  
-          echo  $dbhorse_breed=$row['horse_breed'];
-          echo "<br>"; 
-          echo  $dbhorse_name=$row['skill'];
-          echo "<br>"; 
-          echo  $dbhorse_name=$row['horse_age'];
-          echo "<br>"; 
-          echo  $dbhorse_name=$row['horse_color'];
-          echo "<br>"; 
-          echo  $dbhorse_id=$row['horse_id'];
-          echo "<br>"; 
-          echo  '<form action="" method ="POST"><input type ="hidden" name="horse_id" value='. $dbhorse_id.'>
-          <input type="submit" name= "Retire" value ="Retire"></form>';
-          echo "<br> <br>";  
+          echo  "tr";
+          echo   "<td>" .$dbusername=$row['username']."</td>"; 
+          echo   "<td>" .$dbhorse_name=$row['horse_name']. "</td>";
+          echo   "<td>" .$dbhorse_breed=$row['horse_breed']. "</td>";
+          echo   "<td>" .$dbhorse_name=$row['skill']. "</td>";
+          echo   "<td>" .$dbhorse_name=$row['horse_age']. "</td>";
+          echo   "<td>"  .$dbhorse_name=$row['horse_color']. "</td>";
+          echo   "<td>" .$dbhorse_id=$row['horse_id']."</td>"; 
+          echo  "<td>".'<form action="" method ="POST"><input type ="hidden" name="horse_id" value='. $dbhorse_id.'>
+          <input type="submit" name= "Retire" value ="Retire"></form>'. "</td>";
+          echo "</tr>";
         }
     }
     if(isset($_POST['Retire'])){
