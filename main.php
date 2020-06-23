6b3223b2e49740db700e98a53d13bad458842f11
lalaantika <sharmal0707@gmail.com>
2020-06-23 12:47:37 -0400

    delete function working

--- main.php
+++ main.php
@@ -3,7 +3,7 @@ require_once("connect.php");
     session_start();
     echo "<h1>Hello ... You Made It !!!</h1>";
     $conn = new mysqli('localhost',$user, $pass, $db );
-    $query = "SELECT h.horse_name,h.horse_breed, h.horse_color, h.skill, h.horse_age, u.username from user_horse uh 
+    $query = "SELECT h.horse_id, h.horse_name,h.horse_breed, h.horse_color, h.skill, h.horse_age, u.username from user_horse uh 
     JOIN horse h on h.horse_id =uh.horse_id join user u on uh.user_id = u.user_id 
     where u.user_id ='".$_SESSION ['user_id']."'";
     $result = $conn->query($query);
@@ -20,8 +20,22 @@ require_once("connect.php");
           echo  $dbhorse_name=$row['horse_age'];
           echo "<br>"; 
           echo  $dbhorse_name=$row['horse_color'];
+          echo "<br>"; 
+          echo  $dbhorse_id=$row['horse_id'];
+          echo "<br>"; 
+          echo  '<form action="" method ="POST"><input type ="hidden" name="horse_id" value='. $dbhorse_id.'>
+          <input type="submit" name= "Retire" value ="Retire"></form>';
           echo "<br> <br>";  
         }
+    }
+    if(isset($_POST['Retire'])){
+    $query2 = "DELETE FROM horse where horse_id ='".$_POST['horse_id']."'";
+      if (mysqli_query($conn, $query2)){
+       echo "Record Deleted";
+      }
+      else {
+       echo "Error Unable to Delete Record";
+      }
     }
 ?>
 
