fd0e96cefaa1ec7d8a52558fcc3073f27cbc2a9f
lalaantika <sharmal0707@gmail.com>
2020-06-23 13:42:32 -0400

    with table view and the update function

--- main.php
+++ main.php
@@ -8,20 +8,21 @@ require_once("connect.php");
     where u.user_id ='".$_SESSION ['user_id']."'";
     $result = $conn->query($query);
     echo "Horse list";
-    echo "<table>
-        <tr>
-        <th>Username</th>
-        <th>Horsename</th>
-        <th>Horse Breed</th>
-        <th>Skill</th>
-        <th>Horse Age</th>
-        <th>Horse Color</th>
-        <th>Horse ID</th>
-        <th>Action</th>
-        </tr>";
     if ($result->num_rows > 0) {
+      echo "<table border='1'>
+      <tr>
+      <th>Username</th>
+      <th>Horsename</th>
+      <th>Horse Breed</th>
+      <th>Skill</th>
+      <th>Horse Age</th>
+      <th>Horse Color</th>
+      <th>Horse ID</th>
+      <th>Action</th>
+      <th> Action</th>
+      </tr>";
         while($row = $result->fetch_assoc()) {
-          echo  "tr";
+          echo  "<tr>";
           echo   "<td>" .$dbusername=$row['username']."</td>"; 
           echo   "<td>" .$dbhorse_name=$row['horse_name']. "</td>";
           echo   "<td>" .$dbhorse_breed=$row['horse_breed']. "</td>";
@@ -29,11 +30,16 @@ require_once("connect.php");
           echo   "<td>" .$dbhorse_name=$row['horse_age']. "</td>";
           echo   "<td>"  .$dbhorse_name=$row['horse_color']. "</td>";
           echo   "<td>" .$dbhorse_id=$row['horse_id']."</td>"; 
-          echo  "<td>".'<form action="" method ="POST"><input type ="hidden" name="horse_id" value='. $dbhorse_id.'>
+          echo  "<td>".'<form action="" method ="POST"><input type ="hidden" name="horse_id" value= '. $dbhorse_id.'>
           <input type="submit" name= "Retire" value ="Retire"></form>'. "</td>";
+          echo  "<td>".'<form action="" method ="POST">
+          <input type ="hidden" name="horse_id" value='. $dbhorse_id.'>
+          <input type ="text" name="horsename">
+          <input type="submit" name= "Update" value ="Update"></form>'. "</td>";
           echo "</tr>";
         }
-    }
+        echo "</table>";
+      }
     if(isset($_POST['Retire'])){
     $query2 = "DELETE FROM horse where horse_id ='".$_POST['horse_id']."'";
       if (mysqli_query($conn, $query2)){
@@ -43,5 +49,27 @@ require_once("connect.php");
        echo "Error Unable to Delete Record";
       }
     }
+    if(isset($_POST['Update'])){
+      $query3 = "UPDATE horse SET horse_name = '".$_POST['horsename']."' WHERE horse_id ='".$_POST['horse_id']."'";
+        if (mysqli_query($conn, $query3)){
+         echo "Record Updated";
+        }
+        else {
+         echo "Error Unable to Update Delete Record";
+        }
+      }
 ?>
 
+<style>
+
+table
+{
+border-style:solid;
+border-width:2px;
+border-color:black;
+}
+td{
+  margin:5px;
+  padding:5px;
+}
+</style>
